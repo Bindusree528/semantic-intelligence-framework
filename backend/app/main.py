9217d243c67b22542689210bd6d0e1b8c9a10c94
Bindusree <itsbindusree528@gmail.com>
2025-11-17 14:41:12 +0530

    Updated main.py and other fixes

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     allow_origins=[
     "*",
     "http://localhost:3000",
-    "https://v0-kmrl-document-intelligence-system-3tldaa3sh.vercel.app",
+    "https://semantic-intelligence-framework-q2gp517ez.vercel.app",
     "https://abc123.ngrok-free.app"
   ],
     allow_credentials=True,
